a83c2c788d2fd828a28e213b6195932b2ec08067
Peter Leong <peter.leong@kodak.com>
2019-12-24 11:27:01 -0800

    More Jest testing examples

--- jest-learn/sum.js
+++ jest-learn/sum.js
@@ -1,4 +1,5 @@
 function sum(a, b) {
+    if (typeof(a) === 'string') throw new Error("Strings not allowed");
     return a + b;
 }
 module.exports = sum;
--- jest-learn/sum.test.js
+++ jest-learn/sum.test.js
@@ -5,3 +5,26 @@ describe("My first description", () => {
         expect(sum(1, 2)).toBe(3);
     });
 });
+
+describe("example tests", () => {
+    test('partial match', () => {
+        expect({one: 1, two: 2, three: 3})
+            .toEqual(expect.objectContaining({
+                one: 1,
+                three: 3,
+            }));
+    });
+
+    test('throw error', () => {
+        expect(() => sum("this should", "throw"))
+            .toThrowError(/ not allowed$/);
+    });
+
+    // NOTE: String args still need ${'xxx'}.
+    test.each`
+    a | b | exp
+    ${1} | ${1} | ${2}
+    `("$a + $b = $exp", ({a, b, exp}) => {
+        expect(sum(a, b)).toBe(exp);
+    });
+});
