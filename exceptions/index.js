aba1ef1daf577b374d90be2cfc9edc3fa18d83a5
Peter Leong <peter.leong@kodak.com>
2020-03-19 14:32:53 -0700

    Pseudo 'Casued by' exception attaching

--- exceptions/index.js
+++ exceptions/index.js
@@ -1,6 +1,12 @@
 'use strict'; // this makes previously bad syntax / practices illegal
 
-process.chdir("bruce");
+try {
+    process.chdir("bruce");
+} catch (e) {
+    const alt = new Error('Failed to change to dir "bruce" because: ' + e.message);
+    alt.stack += '\nCaused by: '+ e.stack;
+    throw alt;
+}
 
 // This doesn't print.... I guess errors don't 'resume' by default
 console.log("Still going");
